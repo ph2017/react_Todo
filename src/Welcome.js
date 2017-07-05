d21fb03b3c44ac034a442962f48f7d6ee9bb020a
ph2017 <billa4161@163.com>
2017-07-05 17:40:08 +0800

    第一个react component

---
+++ src/Welcome.js
@@ -0,0 +1,8 @@
+import React from 'react';
+class Welcome extends React.Component {
+    render() {
+        return <h1> Hello, component! </h1>;
+    }
+}
+
+export default Welcome // 为什么要 export，为什么要加 default
--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Welcome from './Welcome';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render( < h1 > Hello, world! < /h1> , document.getElementById('root'));
-        registerServiceWorker();
+/*ReactDOM.render( < h1 > Hello, world! < /h1> , document.getElementById('root'));
+        registerServiceWorker();*/
+
+ReactDOM.render( < Welcome / > , document.getElementById('root'));
