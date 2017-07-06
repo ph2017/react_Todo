1b374f11d61aa83f2b9191edbde8f539444f1e16
ph2017 <billa4161@163.com>
2017-07-06 19:23:38 +0800

    样式更新

--- src/js/Todo/Todo.js
+++ src/js/Todo/Todo.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import 'normalize.css';
+import 'reset.css';
+import '../../css/todo.css'
 import TodoInput from './TodoInput';
 import TodoItem from './TodoItem';
+
 
 class Todo extends React.Component{
     constructor(props){
         super(props);
 
         this.state = {
-            todoList: [{id:1, title:'第1个代办任务'}],
+            todoList: [
+                {id:1, title:'第1个代办任务'},
+                {id:2, title:'第2个代办任务'}
+            ],
             newTodo: ''
         }
     }
@@ -15,7 +22,7 @@ class Todo extends React.Component{
     render(){
 
         return (
-            <div>
+            <div className="todoCt">
                 <h1>我的代办：</h1>
                 <TodoInput content={this.state.newTodo}/> 
                 <TodoItem todoList={this.state.todoList}/>
