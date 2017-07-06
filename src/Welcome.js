34a5ec764d14fe243434e8b2176f0ba85564e479
ph2017 <billa4161@163.com>
2017-07-06 16:28:05 +0800

    生命周期方法测试

--- src/Welcome.js
+++ src/Welcome.js
@@ -6,6 +6,8 @@ class Welcome extends React.Component {
         this.state = {
             date: new Date()
         }
+
+        console.log('我已在constructor里将 props 和 state 初始化好了');
     }
 
     tick(){
@@ -14,8 +16,13 @@ class Welcome extends React.Component {
         });
     }
 
+    componentWillMount(){
+        console.log('运行到这里，说明马上要render了');
+    }
+
     //组件mount时启动定时器
     componentDidMount() {
+        console.log('已经挂载到页面了');
         //保存定时器的timerID到this
         this.timerID = setInterval(
             () => this.tick(),
@@ -25,10 +32,12 @@ class Welcome extends React.Component {
 
     //组件unMount时停止定时器
     componentWillUnmount() {
+        console.log('马上要消失了');
         clearInterval(this.timerID);
     }
 
     render() {
+        console.log('这里是render');
         return (
             <div>
                 <h1> Hello, component! </h1>
