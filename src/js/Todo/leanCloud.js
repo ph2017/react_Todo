08a0febe35523f0ff43c1578dcea2b5f5b628980
ph2017 <billa4161@163.com>
2017-07-07 16:12:44 +0800

    封装leanCloud接口模块

---
+++ src/js/Todo/leanCloud.js
@@ -0,0 +1,10 @@
+import AV from 'leancloud-storage';
+var APP_ID = 'Olxk4NWKifF56MesmP8y5mP6-gzGzoHsz';
+var APP_KEY = 'yOOePg1IUofUKlcxpOSmpY4l';
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+
+export default AV;
